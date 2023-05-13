ed0a2d694d485be1a3c97d4001b3ac919c3e205e
Paul Wanjohi <pwanjohi164@gmail.com>
2023-05-13 17:55:43 +0300

    started creating models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a berief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        return self.title
